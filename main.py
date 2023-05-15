eb1809d702ace34c4d518448147f22ed5c3767b7
Chevyjd2007 <112202840+Chevyjd2007@users.noreply.github.com>
2023-05-15 02:43:45 -0400

    get_games + clean up

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 from requests import get
 from pprint import PrettyPrinter
 
@@ -14,31 +15,38 @@ printer = PrettyPrinter()
 
 
 def get_games(params):
-    response = requests.get(URL, headers=headers, params=params)
-    date = response.json()['parameters']
-    date = date['date']
-    print(f"NBA games on {date[-5:-3]} - {date[-2:]} - {date[:4]}:")
+    try:
+        response = requests.get(URL, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
 
-    for game in response.json()['response']:
-        home = game['teams']['home']['code']
-        away = game['teams']['visitors']['code']
-        score_home = game['scores']['home']['points']
-        score_away = game['scores']['visitors']['points']
-        quarter = game['periods']['current']
-        clock = game['status']['clock']
-        home_nick = game['teams']['home']['nickname']
-        away_nick = game['teams']['visitors']['nickname']
+        date_str = data['parameters']['date']
+        date = datetime.strptime(date_str, "%Y-%m-%d")
+        formatted_date = date.strftime("%B %d, %Y")
+        print(f"NBA games on {formatted_date}:")
 
-        print(home + " vs " + away)
-        if quarter == 4 and clock is not None:
-           print(str(quarter) + ": " + str(score_home) + " - " + str(score_away))
-        elif score_home > score_away:
-            print("Final: " + str(score_home) + " - " + str(score_away) + " --------> " + home_nick + " Win!")
-        else:
-            print("Final: " + str(score_home) + " - " + str(score_away) + " --------> " + away_nick + " Win!")
+        for game in data['response']:
+            home = game['teams']['home']['code']
+            away = game['teams']['visitors']['code']
+            score_home = game['scores']['home']['points']
+            score_away = game['scores']['visitors']['points']
+            quarter = game['periods']['current']
+            clock = game['status']['clock']
+            home_nick = game['teams']['home']['nickname']
+            away_nick = game['teams']['visitors']['nickname']
+
+            print(f"{home} vs {away}")
+            if quarter == 4 and clock is not None:
+                print(f"{quarter}: {score_home} - {score_away}")
+            elif score_home > score_away:
+                print(f"Final: {score_home} - {score_away} --------> {home_nick} Win!")
+            else:
+                print(f"Final: {score_home} - {score_away} --------> {away_nick} Win!")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
 
 
-user_date = input("Welcome! Please specify a date(year-month-day): ")
+user_date = input("Welcome! Please specify a date (year-month-day): ")
 params = {"date": user_date}
-
 get_games(params)
